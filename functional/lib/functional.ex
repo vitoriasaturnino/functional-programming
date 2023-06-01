d4fa6a4582544b8e89bfc765ea84b0e453dde698
vitoriasaturnino <vim.saturnino@gmail.com>
2023-06-01 17:53:50 -0300

    composition of functions

--- functional/lib/functional.ex
+++ functional/lib/functional.ex
@@ -1,8 +1,27 @@
 defmodule Functional do
   def hello do
-    user_data = %{name: "Vitória", age: 23}
+    # Original map
+    %{name: "Vitória", age: 23}
+    |> IO.inspect()
 
-    # cria uma cópia em um novo endereço de memória para a váriável com o valor atualizad
-    user_data = Map.put(user_data, :name, "Raphael")
+    # Map with name changed
+    |> put_map(:name, "Raphael")
+    |> IO.inspect()
+
+    # Map with age updated
+    |> put_map(:age, 25)
+    |> IO.inspect()
+
+    # Map with age deleted
+    |> delete_map_key(:age)
+    |> IO.inspect()
+  end
+
+  defp put_map(map, key, value) do
+    Map.put(map, key, value)
+  end
+
+  defp delete_map_key(map, key) do
+    Map.delete(map, key)
   end
 end
